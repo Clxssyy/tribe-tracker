2182cfef33424eab1c710b74380337d24ea9bd12
Michael Connolly <mpc63@uakron.edu>
2023-11-24 20:23:27 -0500

    Rename variables

--- utils/deploy-commands.js
+++ utils/deploy-commands.js
@@ -7,20 +7,20 @@ require('dotenv').config();
 const deployCommands = async () => {
   const commands = [];
 
-  const foldersPath = path.join(__dirname, '../commands');
-  const commandFolders = fs.readdirSync(foldersPath);
+  const commandsFolder = path.join(__dirname, '../commands');
+  const categories = fs.readdirSync(commandsFolder);
 
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-      .readdirSync(commandsPath)
+  for (const category of categories) {
+    const categoryFolder = path.join(commandsFolder, category);
+    const categoryCommands = fs
+      .readdirSync(categoryFolder)
       .filter((file) => file.endsWith('.js'));
 
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      const command = require(filePath);
-      if ('data' in command && 'execute' in command) {
-        commands.push(command.data.toJSON());
+    for (const command of categoryCommands) {
+      const commandPath = path.join(categoryFolder, command);
+      const newCommand = require(commandPath);
+      if ('data' in newCommand && 'execute' in newCommand) {
+        commands.push(newCommand.data.toJSON());
       } else {
         console.log(
           `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
